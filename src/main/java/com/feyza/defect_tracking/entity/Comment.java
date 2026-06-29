47f02219e9d3bd90b94c951b6c7fc79dbc21fb7d
feyzaakkus <akkusfeyza2005@gmail.com>
2026-06-29 17:30:12 +0300

    Temel entity sınıfları oluşturuldu

---
+++ src/main/java/com/feyza/defect_tracking/entity/Comment.java
@@ -0,0 +1,4 @@
+package com.feyza.defect_tracking.entity;
+
+public class Comment {
+}
---
+++ src/main/java/com/feyza/defect_tracking/entity/User.java
@@ -0,0 +1,21 @@
+package com.feyza.defect_tracking.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
