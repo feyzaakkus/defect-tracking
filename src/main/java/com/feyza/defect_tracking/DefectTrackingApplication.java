7ee31c131734744ac3b2d29fc480b6ae05381906
feyzaakkus <akkusfeyza2005@gmail.com>
2026-06-29 11:08:06 +0300

    Initial commit

---
+++ src/main/java/com/feyza/defect_tracking/DefectTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.feyza.defect_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefectTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefectTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feyza/defect_tracking/DefectTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feyza.defect_tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefectTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
